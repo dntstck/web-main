c05c81034666532b692b1b9d1e65a9a8be84b5fd
dntstck <drudelarosa@icloud.com>
2025-01-20 01:36:01 +0000

    updated display.js

--- assets/display.js
+++ assets/display.js
@@ -1,19 +1,18 @@
-const navElement = document.getElementById('nav');
+document.addEventListener("DOMContentLoaded", function () {
+  const navElement = document.getElementById("nav");
 
-if (navElement){
-  console.log("nav found");
-  
+  if (navElement) {
+    console.log("nav found");
 
-  navElement.addEventListener('mouseover', function() { 
-  navElement.style.opacity = '0.8';
-  navElement.style.transition = 'opacity 0.3s ease'
-  });
+    navElement.addEventListener("mouseover", function () {
+      navElement.style.opacity = "0.8";
+      navElement.style.transition = "opacity 0.3s ease";
+    });
 
-navElement.addEventListener('mouseout', function() {
-   navElement.style.opacity = '1'; 
-  });
-
-} else{
-  console.log("nav not found");
-  
-}
+    navElement.addEventListener("mouseout", function () {
+      navElement.style.opacity = "1";
+    });
+  } else {
+    console.log("nav not found");
+  }
+});

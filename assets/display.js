5fab958cc5f13769734511a2d70c261760d4828f
dntstck <drudelarosa@icloud.com>
2025-01-20 01:32:58 +0000

    updated display.js

--- assets/display.js
+++ assets/display.js
@@ -1,10 +1,19 @@
 const navElement = document.getElementById('nav');
 
-navElement.addEventListener('mouseover', function() { 
+if (navElement){
+  console.log("nav found");
+  
+
+  navElement.addEventListener('mouseover', function() { 
   navElement.style.opacity = '0.8';
   navElement.style.transition = 'opacity 0.3s ease'
-});
+  });
 
 navElement.addEventListener('mouseout', function() {
    navElement.style.opacity = '1'; 
-});
+  });
+
+} else{
+  console.log("nav not found");
+  
+}
